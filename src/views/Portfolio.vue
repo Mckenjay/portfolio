3102482416366dc7d7f5be5e10440ac093d72f26
Mckenjay <tedbryanrazonado12345@gmail.com>
2026-09-20 22:24:36 +0800

    Refactor layout styles and update portfolio view

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+    import NavBar from '@/components/NavBar.vue';
+    import HomeSection from '@/components/sections/HomeSection.vue';
 </script>
 
 <template>
-    
+    <NavBar />
+    <HomeSection />
 </template>
 
 <style scoped></style>
